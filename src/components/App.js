9f2b06dc7e362cef7e03e549c5f1f427b8e007c7
misterKumar <120304503+misterKumar@users.noreply.github.com>
2023-09-05 17:17:41 +0530

    submitting code-Tue Sep 05 2023 17:17:39 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import Autocomplete from "./Autocomplete";
 const App = () => {
   return (
     <div>
--- src/components/Autocomplete.js
+++ src/components/Autocomplete.js
@@ -1,21 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import "../styles/App.css"
 
 const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
 
 function Autocomplete() {
   const [inputValue, setInputValue] = useState('');
   const [suggestions, setSuggestions] = useState([]);
+  const [debouncedInput, setDebouncedInput] = useState('');
+
+  useEffect(() => {
+    // Debounce the input to prevent excessive filtering while typing
+    const timeoutId = setTimeout(() => {
+      setDebouncedInput(inputValue);
+    }, 300); // Adjust the debounce delay as needed
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [inputValue]);
+
+  useEffect(() => {
+    // Filter the suggestions based on the debounced input value
+    const filteredSuggestions = fruits.filter((fruit) =>
+      fruit.toLowerCase().includes(debouncedInput.toLowerCase())
+    );
+
+    setSuggestions(filteredSuggestions);
+  }, [debouncedInput]);
 
   const handleInputChange = (e) => {
     const value = e.target.value;
     setInputValue(value);
-
-    // Filter the suggestions based on the input value
-    const filteredSuggestions = fruits.filter((fruit) =>
-      fruit.toLowerCase().includes(value.toLowerCase())
-    );
-
-    setSuggestions(filteredSuggestions);
   };
 
   const handleSuggestionClick = (suggestion) => {
@@ -27,10 +42,11 @@ function Autocomplete() {
 
   return (
     <div>
-      <h1>Autocomplete Example</h1>
+      <h1>Search Item</h1>
       <input
         type="text"
         placeholder="Type a fruit name"
+        className="in"
         value={inputValue}
         onChange={handleInputChange}
       />
@@ -46,3 +62,4 @@ function Autocomplete() {
 }
 
 export default Autocomplete;
+
